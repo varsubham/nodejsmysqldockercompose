d85b32c640521555a5fe8e0af57c86c61a54abb3
varsubham <var.subham@gmail.com>
2021-03-22 17:32:53 +0000

    initial commit

---
+++ web/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+
+const connection = mysql.createPool({
+	connectionLimit: 10,
+	host: process.env.MYSQL_HOST || 'localhost',
+	user: process.env.MYSQL_USER || 'root',
+	password: process.env.MYSQL_PASSWORD || 'password',
+	database: process.env.MYSQL_DATABASE || 'test'
+});
+
+app.get('/', (req, res) => {
+	connection.query('SELECT * FROM Student' , (err, rows) => {
+		if(err){
+			res.json({
+				success: false,
+				err
+				});
+		}
+		else{
+			res.json({
+				success: true,
+				rows
+				});
+		}
+	});
+});
+
+app.listen(5000, () => console.log('listining on port 5000'));
